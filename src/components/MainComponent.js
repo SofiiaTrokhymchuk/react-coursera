8d1758f3c50779690a3b916fb490d056b0c7dea1
Sofiia Trokhymchuk <s.s.trokhymchuk@gmail.com>
2022-05-16 22:11:06 +0300

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { LEADERS } from "../shared/leaders";
 import { PROMOTIONS } from "../shared/promotion";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 
 class Main extends Component {
    constructor(props) {
@@ -72,6 +73,11 @@ class Main extends Component {
                />
                <Route path={"/menu/:dishId"} component={DishWithId} />
                <Route exact path={"/contactus"} component={Contact} />
+               <Route
+                  exact
+                  path={"/aboutus"}
+                  component={() => <About leaders={this.state.leaders} />}
+               />
                <Redirect to={"/home"} />
             </Switch>
             <Footer />
