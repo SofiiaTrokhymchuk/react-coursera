4cce58c3d08172903d9ca654d1f77ad3f45cadd3
Sofiia Trokhymchuk <s.s.trokhymchuk@gmail.com>
2022-05-20 22:25:55 +0300

    React Redux Forms Revisited

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -11,24 +11,26 @@ import { Link } from "react-router-dom";
 
 function RenderLeader({ leader }) {
    return (
-      <div key={leader.id} className="col-12 mt-5">
-         <Media tag="li">
-            <Media left middle>
-               <Media object src={leader.image} alt={leader.name} />
-            </Media>
-            <Media body className="ml-5">
-               <Media heading>{leader.name}</Media>
-               <p>{leader.designation}</p>
-               <p>{leader.description}</p>
-            </Media>
+      <Media tag="li">
+         <Media left middle>
+            <Media object src={leader.image} alt={leader.name} />
          </Media>
-      </div>
+         <Media body className="ml-5">
+            <Media heading>{leader.name}</Media>
+            <p>{leader.designation}</p>
+            <p>{leader.description}</p>
+         </Media>
+      </Media>
    );
 }
 
 function About(props) {
    const leaders = props.leaders.map((leader) => {
-      return <RenderLeader leader={leader} />;
+      return (
+         <div key={leader.id} className="col-12 mt-5">
+            <RenderLeader leader={leader} />
+         </div>
+      );
    });
 
    return (
