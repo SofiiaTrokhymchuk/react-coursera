6a3139b4720a2e992d3f8b8240ea9228deb52841
Sofiia Trokhymchuk <s.s.trokhymchuk@gmail.com>
2022-05-21 22:44:12 +0300

    Fetch Post Comment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -44,7 +44,7 @@ class CommentForm extends Component {
 
    handleSubmit(values) {
       this.toggleModal();
-      this.props.addComment(
+      this.props.postComment(
          this.props.dishId,
          values.rating,
          values.author,
@@ -159,7 +159,7 @@ function RenderDish({ dish }) {
    }
 }
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
    if (comments != null) {
       const commentsList = comments.map((comment) => {
          return (
@@ -181,7 +181,7 @@ function RenderComments({ comments, addComment, dishId }) {
          <div className="col-12 col-md-5 m-1">
             <h4>Comments</h4>
             <ul className="list-unstyled">{commentsList}</ul>
-            <CommentForm dishId={dishId} addComment={addComment} />
+            <CommentForm dishId={dishId} postComment={postComment} />
          </div>
       );
    } else {
@@ -226,7 +226,7 @@ const DishDetail = (props) => {
                <RenderDish dish={props.dish} />
                <RenderComments
                   comments={props.comments}
-                  addComment={props.addComment}
+                  postComment={props.postComment}
                   dishId={props.dish.id}
                />
                <div></div>
